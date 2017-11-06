85baf3acb7ffb2543c98c31bb33668abbc61d0e8
seechinsu <seechinsu@gmail.com>
2017-11-05 20:56:51 -0500

    - refactored put and delete method to use sqlite3 - refactored ItemList class to use sqlite3 - created class methods for find_by_name, update, and insert

--- item.py
+++ item.py
@@ -26,6 +26,30 @@ class Item(Resource):
         if row:
             return {"item": {"name": row[0], "price": row[1]}}
 
+    @classmethod
+    def insert(cls, item):
+        global db_home
+        connection = sqlite3.connect(db_home)
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?,?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
+    @classmethod
+    def update(cls, item):
+        global db_home
+        connection = sqlite3.connect(db_home)
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price = ? WHERE name = ?"
+        cursor.execute(query, (item['price'], item['name'],))
+
+        connection.commit()
+        connection.close()
+
     @jwt_required()
     def get(self, name):
         item = Item.find_by_name(name)
@@ -41,36 +65,64 @@ class Item(Resource):
 
         item = {"name": name, "price": data['price']}
 
-        global db_home
-        connection = sqlite3.connect(db_home)
-        cursor = connection.cursor()
-
-        query = "INSERT INTO items VALUES (?,?)"
-        cursor.execute(query, (item['name'], item['price']))
-
-        connection.commit()
-        connection.close()
+        try:
+            Item.insert(item)
+        except:
+            return {"message": "An error occurred inserting the item."}, 500
 
         return item, 201
 
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
+        global db_home
+        if Item.find_by_name(name):
+            connection = sqlite3.connect(db_home)
+            cursor = connection.cursor()
+
+            query = "DELETE FROM items WHERE name = ?"
+            cursor.execute(query, (name,))
+
+            connection.commit()
+            connection.close()
+
+            return {"message": name + " has been deleted"}
+        return {"message": name + " is not found"}
 
     def put(self, name):
+        global db_home
+
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = Item.find_by_name(name)
+
+        updated_item = {"name": name, "price": data['price']}
+
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-            return {'message': 'item has been added'}
+            try:
+                Item.insert(updated_item)
+            except:
+                return {"message": "An error occurred inserting the item."}, 500
         else:
-            item.update(data)
-            return {'message': 'item has been updated'}
+            try:
+                Item.update(updated_item)
+            except:
+                return {"message": "An error occurred updating the item."}, 500
+        return updated_item
 
 
 class ItemList(Resource):
     def get(self):
+        global db_home
+        connection = sqlite3.connect(db_home)
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+
+        items = []
+
+        for row in result:
+            items.append({"name": row[0], "price": row[1]})
+
+        connection.close()
+
         return {'items': items}
